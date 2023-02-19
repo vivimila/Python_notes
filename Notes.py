88180551a17eb1c8b3ab72d4a87bea8673ab0e93
vivimila <violamilashevskaya@yandex.by>
2023-02-19 21:32:01 +0300

    Add function delete

--- Notes.py
+++ Notes.py
@@ -25,6 +25,19 @@ def open_file():
     global file_name
     int = askopenfile(mode='r')
 
+def delete_note(self): 
+        #self.last = None
+        if len(self.notesList.selectedItems()) != 0:
+            note_name = self.notesList.selectedItems()[0].text(0)
+            if note_name.strip() == "":
+                note_name = "untitled"
+            self.notesList.takeTopLevelItem(
+                self.notesList.indexOfTopLevelItem(
+                    self.notesList.selectedItems()[0]
+                    )
+                )
+            self.notes_dict.pop(note_name)
+
 root = Tk()
 root.title("Заметки")
 root.geometry("500x400")
@@ -37,6 +50,7 @@ file_menu = Menu(menu_bar)
 file_menu.add_command(label="Создать", command=new_file)
 file_menu.add_command(label="Открыть", command=open_file)
 file_menu.add_command(label="Сохранить", command=save_file)
+file_menu.add_command(label="Удалить", command=delete_note)
 menu_bar.add_cascade(label="Файл", menu=file_menu)
 
 root.config(menu=menu_bar)
